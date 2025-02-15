1d3e1cfd885a82b8e128de55b60e96a8f323ac32
MeTerminator <42143735+MeTerminator@users.noreply.github.com>
2025-02-15 13:54:50 +0800

    Init: WiP Notice

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -35,27 +35,27 @@ export default function Footer() {
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
-            href="https://github.com/vercel/next.js"
+            href="https://github.com/MeTerminator/met-toolbox/"
           >
             <ArrowIcon />
-            <p className="ml-2 h-7">github</p>
-          </a>
-        </li>
-        <li>
-          <a
-            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-            rel="noopener noreferrer"
-            target="_blank"
-            href="https://vercel.com/templates/next.js/portfolio-starter-kit"
-          >
-            <ArrowIcon />
-            <p className="ml-2 h-7">view source</p>
+            <p className="ml-2 h-7">Github</p>
           </a>
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} MIT Licensed
+        © {new Date().getFullYear()} MeTerminator
       </p>
+      <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://met6.top/"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">MeT-Home</p>
+          </a>
+        </li>
     </footer>
   )
 }
